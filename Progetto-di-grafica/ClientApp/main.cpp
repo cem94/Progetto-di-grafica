3c271c91167d357a8fe5724f0f820a176fdc6ebb
GREGORIO <gregorio.meyer@student.supsi.ch>
2018-12-19 16:22:58 +0100

    Added correct glm library

---
+++ Progetto-di-grafica/ClientApp/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Engine.h"
+Engine* engine = new Engine();
+int main(int argc, char * argv[])
+{
+    std::cout << "Client application starts"<<std::endl;
+    //TODO sistemare dipendenze
+    engine->initEngine(1920,1080);
+    //engine->renderMain(argc,argv);
+    //engine->startLoop();
+    //TODO: set alla Callback
+    //glutDisplayFunc(displayCallback);
+    //engine->reshape(reshapeCallback);
+    //glutSpecialFunc(specialCallback);
+    //glutKeyboardFunc(specialCallback);
+    //glutMouseFunc(mouseCallback);
+    //glutMotionFunc(dragCallback);
+    std::cout << "Application terminated"<<std::endl;
+}
